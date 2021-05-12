8b6b73dc4bb0014b45a68a3616a132e09789225b
vinicius <Vinisilva2001@hotmail.com>
2021-05-12 15:48:11 -0300

    testing json with postman (post)

--- config/customExpress.js
+++ config/customExpress.js
@@ -1,10 +1,14 @@
-const { response } = require('express')
 const express = require('express')
 const consing = require('consign')
+const bodyParser = require('body-parser')
 
 
 module.exports = () => {
-    const app = express()   
+    const app = express()  
+    
+    app.use(bodyParser.urlencoded({extended: true}))
+    app.use(bodyParser.json())
+
 
     consing()
      .include('controllers')
